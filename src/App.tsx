0e3c2128a56a232d2a1a6f97f70339e8b38bf87a
julianmcapicua <julian@capicua.com.uy>
2021-08-21 02:32:01 -0300

    working version without codegen

--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,70 @@ import {
   HttpLink,
   InMemoryCache
 } from '@apollo/client'
+import { Button, createTheme, ThemeProvider } from '@material-ui/core'
 import React from 'react'
 import './App.css'
-import { useGetTestQuery } from './generated/graphql'
 
 const createApolloClient = () => {
   return new ApolloClient({
     link: new HttpLink({
       uri: process.env.HASURA_ENDPOINT,
+
       headers: {
         'x-hasura-admin-secret': process.env.X_HASURA_ADMIN_SECRET
       }
     }),
+
     cache: new InMemoryCache()
   })
 }
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#80e27e',
+
+      main: '#4caf50',
+
+      dark: '#087f23',
+
+      contrastText: '#fff'
+    },
+
+    secondary: {
+      light: '#cfcfcf',
+
+      main: '#9e9e9e',
+
+      dark: '#707070',
+
+      contrastText: '#000'
+    }
+  }
+})
 
 function App() {
   const client = createApolloClient()
 
   return (
     <ApolloProvider client={client}>
-      <div className='App' />
-      <Test />
+      <ThemeProvider theme={theme}>
+        <div className='App' />
+
+        <Test />
+      </ThemeProvider>
     </ApolloProvider>
   )
 }
 
 const Test = () => {
-  const { data } = useGetTestQuery()
-
-  return <div>{data?.test.map((i) => i.text)}</div>
+  return (
+    <div>
+      <Button color='secondary' variant='contained'>
+        Boton
+      </Button>
+    </div>
+  )
 }
 
 export default App
